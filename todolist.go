b31187bf8ec575fa429e92172c4e9237c8a6cb36
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-05 12:36:13 +0700

    fix typo

--- todolist.go
+++ todolist.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func Health(w http.ResponseWriter, r *http.Request) {
+func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
